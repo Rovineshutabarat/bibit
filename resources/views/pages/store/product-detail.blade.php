bd3ed8bb8b2e1c2ac50da7b274ef6a50c1665622
j35u5212 <andreasdoloksaribu7@gmail.com>
2024-12-03 10:30:22 +0700

    add: deskripsi produk

--- resources/views/pages/store/product-detail.blade.php
+++ resources/views/pages/store/product-detail.blade.php
@@ -101,20 +101,10 @@
 
                     <div class="mt-8">
                         <h3 class="text-lg font-bold text-gray-800">Product Description</h3>
-                        <p class="text-sm text-gray-600 mt-4">Step up your footwear game with our premium men's shoes.
-                            Designed for comfort and crafted with a contemporary aesthetic, these versatile shoes are a
-                            must-have addition to your wardrobe. The supple and breathable materials ensure all-day comfort,
-                            making them perfect for everyday wear.</p>
+                        <p class="text-sm text-gray-600 mt-4">{{ $product->description }}</p>
                     </div>
 
-                    <ul class="space-y-3 list-disc mt-6 pl-4 text-sm text-gray-600">
-                        <li>A pair of gray shoes is a wardrobe essential due to its versatility.</li>
-                        <li>Available in a wide range of sizes, from extra small to extra large, and even in tall and petite
-                            sizes.</li>
-                        <li>Easy to maintain, they can be machine-washed and dried on low heat.</li>
-                        <li>Personalize them with your own designs, patterns, or embellishments to make them uniquely yours.
-                        </li>
-                    </ul>
+                    
                 </div>
             </div>
         </div>
