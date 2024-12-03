f7095aec779f85bcfca9e0423652dc555b60a148
j35u5212 <andreasdoloksaribu7@gmail.com>
2024-12-03 10:44:00 +0700

    add: landing page upgrade dikit

--- resources/views/components/main/navbar.blade.php
+++ resources/views/components/main/navbar.blade.php
@@ -8,6 +8,7 @@
                 <a href=""
                     class="hover:text-green-200 @if(request()->routeIs('home')) text-green-200 @endif">Beranda</a>
                 <a href="{{ route('auth.login') }}" class="hover:text-green-200">Login</a>
+                <a href="{{ route('store.index') }}" class="hover:text-green-200 @if(request()->routeIs('store.index')) text-green-200 @endif">Store</a>
             </div>
         </div>
     </div>
